7e32f969053ac4a0704b782d028524c7b2993c74
Reydel Ocon <reydel321@gmail.com>
2024-02-20 06:53:58 +0800

    change ui login

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,7 +3,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@radix-ui/react-label'
 import axios from 'axios'
 import { useState } from 'react'
-
+import Farmer from '@/assets/farmer.png'
 import { UserType } from '@/entities/types'
 import { Navigate } from 'react-router-dom'
 
@@ -51,33 +51,44 @@ export default function Login() {
       })
   }
   return (
-    <div className="w-dvw h-dvh flex justify-center items-center flex-col ">
-      <div className="bg-white shadow-slate-400 shadow-sm my-[2rem] w-[35rem] h-fit flex justify-center items-center flex-col p-4 rounded-md">
-        <h1 className="my-[1rem] font-bold">Please log in</h1>
-        <Label className="mb-1 self-start text-sm">Username</Label>
+    <div className="w-dvw h-dvh flex justify-between items-center flex-row ">
+      <div className="bg-[#D04848] text-[#F3B95F] shadow-slate-400 w-[40%] px-[5rem] h-full flex justify-center items-center flex-col p-4 rounded-md">
+        <h1 className="mb-[7rem] font-semibold text-3xl">
+          CROP MANAGEMENT AND HARVESTING SCHEDULE SYSTEM
+        </h1>
+        {/* <Label className="mb-1 self-start text-sm">Username</Label> */}
         <Input
           onChange={handleChange}
-          className="mb-2"
+          className="mb-8 border-4 border-[#F3B95F] rounded-full p-8 w-full text-[#F3B95F] focus:outline-none placeholder:text-[#F3B95F] placeholder:text-2xl placeholder:font-semibold"
           placeholder="Username"
           name="username"
         />
 
-        <Label className="mb-1 self-start text-sm">Password</Label>
+        {/* <Label className="mb-1 self-start text-sm">Password</Label> */}
         <Input
+          className="mb-2 border-4 border-[#F3B95F] rounded-full p-8 w-full text-[#F3B95F] focus:outline-none placeholder:text-[#F3B95F] placeholder:text-2xl placeholder:font-semibold"
           type="password"
           onChange={handleChange}
           name="password"
           placeholder="Password"
         />
-        <Button
+
+        <a
           onClick={handleLogin}
-          className="mt-[2rem] w-[10rem]"
-          variant="outline"
+          className="text-[#F3B95F] bg-inherit text-3xl outline-none font-semibold cursor-pointer my-[1.5rem] hover:text-white"
         >
           Login
-        </Button>
+        </a>
 
         {errorInput && <p className="text-red-500">{errorInput}</p>}
+      </div>
+
+      <div className="w-[60%] bg-[#F3B95F] h-full items-center flex justify-center relative">
+        <img className="w-[80%] absolute" src={Farmer} alt="farmer" />
+
+        <div className="w-full h-full flex justify-end items-center">
+          <div className="bg-[#6895D2] w-[50%] h-[80%]"></div>
+        </div>
       </div>
     </div>
   )
