15ec9c6b79d5ee34c8e269d61207b70284a60ae7
Reydel Ocon <reydel321@gmail.com>
2024-02-23 14:07:07 +0800

    oh sheshh

--- src/pages/ManageCrops.tsx
+++ src/pages/ManageCrops.tsx
@@ -6,12 +6,21 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
 import { Link } from 'react-router-dom'
 import { FaArrowCircleRight } from 'react-icons/fa'
 import Search from '@/lib/Search'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-
+import ButtonStyle from '@/lib/ButtonStyle'
 type ResponseData = {
   crops_id: string
   crops_name: string
@@ -19,12 +28,20 @@ type ResponseData = {
   field_id: string
   field_name: string
   suitability: string
+  suitable_month: string
 }
 
 export default function ManageCrops() {
   const [search, setSearch] = useState('')
   const user_id = localStorage.getItem('cmhs_token')
   const [responseData, setResponseData] = useState<ResponseData[]>([])
+  const [filterMonth, setFilterMonth] = useState('' as string)
+  const [sortCrops, setSortCrops] = useState(false)
+
+  const handleMonth = (e: string) => {
+    setFilterMonth(e)
+    console.log(e)
+  }
 
   const fetchCropsFromFirstPage = async () => {
     await axios
@@ -42,6 +59,22 @@ export default function ManageCrops() {
       })
   }
 
+  const [sortOrder, setSortOrder] = useState('asc')
+
+  const toggleSortOrder = () => {
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc'
+    setSortOrder(newSortOrder)
+  }
+
+  const sortedData = [...responseData].sort((a, b) => {
+    // Sort by crops name
+    if (sortOrder === 'asc') {
+      return a.crops_name.localeCompare(b.crops_name)
+    } else {
+      return b.crops_name.localeCompare(a.crops_name)
+    }
+  })
+
   useEffect(() => {
     fetchCropsFromFirstPage()
   }, [])
@@ -56,43 +89,97 @@ export default function ManageCrops() {
         <Search onChange={(e) => setSearch(e.target.value)} />
       </div>
 
-      <div className="w-full flex justify-center items-center">
-        <div className="w-[80%]">
-          <Table className="w-full">
+      {/* filter buttons  */}
+
+      <div className="w-full flex justify-center items-center mt-[3rem] flex-col">
+        <div className="w-[80%] flex h-[3.5rem] gap-2 items-end justify-end mb-4">
+          <Button
+            onClick={toggleSortOrder}
+            className="rounded-full h-full bg-primary-yellow font-bold text-xl text-primary-red hover:bg-primary-red hover:text-primary-yellow hover:border-primary-yellow hover:border-4"
+          >
+            {sortOrder === 'asc' ? 'Sort Descending' : 'Sort Ascending'}
+          </Button>
+          <Select required onValueChange={(e: string) => handleMonth(e)}>
+            <SelectTrigger className="w-[20%] h-full bg-primary-red text-primary-yellow border-4 border-primary-yellow font-bold rounded-full">
+              <SelectValue placeholder="Month" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="All">All</SelectItem>
+              <SelectItem value="January">January</SelectItem>
+              <SelectItem value="February">February</SelectItem>
+              <SelectItem value="March">March</SelectItem>
+              <SelectItem value="April">April</SelectItem>
+              <SelectItem value="May">May</SelectItem>
+              <SelectItem value="June">June</SelectItem>
+              <SelectItem value="July">July</SelectItem>
+              <SelectItem value="August">August</SelectItem>
+              <SelectItem value="September">September</SelectItem>
+              <SelectItem value="October">October</SelectItem>
+              <SelectItem value="November">November</SelectItem>
+              <SelectItem value="December">December</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="w-[80%] border-4 border-primary-yellow p-8 rounded-[5rem]">
+          <Table className="w-full text-lg ">
             <TableHeader>
-              <TableRow className="text-primary-yellow">
-                <TableHead className="text-primary-yellow text-xl">
+              <TableRow className="text-primary-yellow border-none">
+                <TableHead className="text-primary-yellow text-2xl">
                   Crop ID
                 </TableHead>
-                <TableHead className="text-primary-yellow text-xl">
+                <TableHead className="text-primary-yellow text-2xl">
                   Suitability
                 </TableHead>
-                <TableHead className="text-primary-yellow text-xl">
+                <TableHead className="text-primary-yellow text-2xl">
                   Crop Name
                 </TableHead>
 
-                <TableHead className="text-primary-yellow text-xl">
+                <TableHead className="text-primary-yellow text-2xl">
                   Status
                 </TableHead>
 
-                <TableHead className="text-primary-yellow text-xl">
-                  Field ID
+                <TableHead className="text-primary-yellow text-2xl">
+                  Field Name
                 </TableHead>
               </TableRow>
             </TableHeader>
-            <TableBody className="border-4 border-primary-yellow">
+            <TableBody>
               {responseData.length > 0 ? (
-                responseData.map((crop, index) => {
-                  return (
-                    <TableRow key={index} className="text-white">
-                      <TableCell>{crop.crops_id}</TableCell>
-                      <TableCell>{crop.suitability}</TableCell>
-                      <TableCell>{crop.crops_name}</TableCell>
-                      <TableCell>Planted</TableCell>
-                      <TableCell>{crop.field_id}</TableCell>
-                    </TableRow>
+                sortedData
+                  .filter(
+                    (res) =>
+                      res.suitable_month.includes(filterMonth) ||
+                      (filterMonth === 'All' &&
+                        res.crops_name
+                          .toLowerCase()
+                          .includes(search.toLowerCase())),
                   )
-                })
+                  .map((res, index) => {
+                    return (
+                      <TableRow
+                        key={index}
+                        className="text-primary-yellow border-none"
+                      >
+                        <TableCell>{res.crops_id}</TableCell>
+                        <TableCell className="flex gap-2 items-center ">
+                          <span
+                            className={`w-[1rem] block h-[1rem] border-2 rounded-full ${
+                              res.suitability.includes('High') &&
+                              res.suitability === 'Suitable'
+                                ? 'bg-green-500'
+                                : res.suitability.includes('Moderate')
+                                ? 'bg-yellow-500'
+                                : 'bg-red-500'
+                            }`}
+                          ></span>
+                          {res.suitability}
+                        </TableCell>
+                        <TableCell>{res.crops_name}</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell>{res.field_name}</TableCell>
+                      </TableRow>
+                    )
+                  })
               ) : (
                 <TableRow className="text-white">
                   <TableCell colSpan={5} className="text-center">
@@ -103,6 +190,7 @@ export default function ManageCrops() {
             </TableBody>
           </Table>
         </div>
+        S
       </div>
 
       <div className="absolute right-5 bottom-[50%]">
